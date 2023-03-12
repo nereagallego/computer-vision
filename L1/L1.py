6a9b6e65de28cd0f9a780f68af99c930212974dd
Nerea <801950@unizar.es>
2023-03-12 17:50:16 +0100

    Update L1.py

--- L1/L1.py
+++ L1/L1.py
@@ -2,6 +2,11 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 #cap = cv2.VideoCapture(0)
+
+"""_summary_ contrasta una imagen
+    Args:
+        img: imagen a la que se le aplica el contraste
+"""
 def contraste(img):
     lab= cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
     l_channel, a, b = cv2.split(lab)
@@ -20,14 +25,22 @@ def contraste(img):
     # Stacking the original image with the enhanced image
     return enhanced_img
 
-
-
+"""_summary_ ecualizar una imagen
+    Args:
+        img: imagen que se va a ecualizar
+"""
 def equalizar(img):
     img[:,:,0] = cv2.equalizeHist(img[:,:,0])
     img[:,:,2] = cv2.equalizeHist(img[:,:,2])
     return img
 
-
+"""_summary_ pone una máscara de alien
+    Args:
+        img: imagen sobre la que se aplica la máscara
+        rgbColor: imagen del mismo tamaño que la anterior con el color que se quiere aplicar a la máscara
+    Returns:
+        devuelve una imagen detectando la piel de una persona y cambiándolo por el color de la máscara
+"""
 def alien(img,rgbColor):
    
 
@@ -44,21 +57,32 @@ def alien(img,rgbColor):
 
     # Bitwise-AND mask and original image
     result = cv2.bitwise_and(hsvColor,hsvColor, mask= mask)
-# =============================================================================
-#     cv2.imshow('Mask',mask)
-#     cv2.waitKey(0)
-# =============================================================================
+
     mask = cv2.cvtColor(result, cv2.COLOR_HSV2BGR)
-    cv2.addWeighted(img, 0.2, mask, 0.8, 0, result)
+    cv2.addWeighted(img, 0.4, mask, 0.6, 1, result)
 
     return result
 
+"""_summary_ reduce el numero de colores de una imagen
+    Args:
+        img: imagen a la que se va a hacer poster.
+    Returns:
+        devuelve la imagen original con el espacio de colores reducido
+"""
 def poster(img):
     img[img >= 170]= 255
     img[img < 85] = 0
     img[np.bitwise_and(img > 85, img < 170)] = 128
     return img
 
+"""_summary_ Aplica distorsión radial. Depende de K1 y K2 se consiguen distorsiones de barril o cojin.
+    Args:
+        img: imagen que se va a distorsionar
+        K1: cantidad de distorsión de pixeles al cuadrado
+        K2: cantidad de distorsión de pixeles a la cuarta
+    Returns:
+        devuelve la imagen original con la distorsión aplicada.
+"""
 def barrel(img, K1: float, K2: float):
     xcent, ycent = int(img.shape[1]/2), int(img.shape[0]/2)
     map_x = np.zeros((img.shape[0], img.shape[1]), dtype=np.float32)
@@ -71,6 +95,13 @@ def barrel(img, K1: float, K2: float):
     dst = cv2.remap(img, map_x, map_y, cv2.INTER_LINEAR)
     return dst
 
+
+"""_summary_ Aplica filtro gaussiano a la imagen que se le pasa.
+    Args:
+        img: imagen a la que se va a aplicar el filtro gaussiano.
+    Returns:
+        devuelve la imagen original con el filtro gaussiano aplicado.
+"""
 def gaussianFilter(img):
     blur = cv2.GaussianBlur(img,(5,5),0)
     return blur
@@ -92,7 +123,6 @@ def leerOpcion():
     return opcion
 cap = cv2.VideoCapture(0)
 leido, img = cap.read()
-#img = cv2.imread('color.jpg', cv2.IMREAD_COLOR)
 img2 = cv2.imread('lena.jpg', cv2.IMREAD_GRAYSCALE)
 
 leido = True
@@ -101,7 +131,7 @@ if leido == True:
     
     opcion = leerOpcion()
     while opcion != 0:
-	#cv2.imwrite("foto.png", frame)
+
         if opcion == 1:
             cap = cv2.VideoCapture(0)
             while True:
@@ -149,7 +179,7 @@ if leido == True:
                     break
                 # Our operations on the frame come here
                 img_copy = np.zeros(frame.shape, np.uint8);
-                img_copy[:,:,0] = 255
+                img_copy[:,:,2] = 255
                 result = alien(frame,img_copy)
                 Hori = np.concatenate((frame, result), axis=1)
                 # Display the resulting frame
