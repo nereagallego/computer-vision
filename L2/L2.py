127cfc42afe3036a8c170ec25ae6d6229471c60a
Nerea <801950@unizar.es>
2023-03-13 23:36:57 +0100

    canny operator

--- L2/L2.py
+++ L2/L2.py
@@ -53,13 +53,66 @@ def SobelOperator(imagen):
     cv2.waitKey(0)
 
 
- #def cannyOperator(img):
+def gaussiana(sigma):
+    n = 5 * sigma if sigma % 2 ==1 else 5 * sigma + 1
+    G = np.zeros(n, float)
+    inf_limit = int(-((n -1) /2))
+    sup_limit = int((n-1)/ 2+1)
+    i = 0
+    for x in range(inf_limit,sup_limit, 1):
+       G[i] = math.exp(-((x **2)/ (2*(sigma **2))))
+       i = i +1
+    return G
+
+def gaussianaDerivada(sigma):
+    n = 5 * sigma if sigma % 2 ==1 else 5 * sigma + 1
+    G = np.zeros(n, float)
+    inf_limit = int(-((n -1) /2))
+    sup_limit = int((n-1)/ 2+1)
+    i = 0
+    for x in range(inf_limit,sup_limit, 1):
+       G[i] = - x / (sigma ** 2) * math.exp((- (x ** 2)) / (2 * (sigma **2)))
+       i = i +1
+    return G
+
+def K(G, G2):
+    suma = 0
+    for valor in G:
+        if valor > 0:
+            suma += valor 
+    for valor in G2:
+        if valor > 0:
+            suma += valor
+    return suma
+
+def cannyOperator(img):
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    G = gaussiana(1)
+    G2 = gaussianaDerivada(1)
+    k = K(G,G2)
+    mask_x = G2[:, None] @ G[None,:]
+    mask_x = mask_x * 1 / k
+    mask_y = G[:,None] @ G2[None,:]
+    mask_y = mask_y * 1 / k
     
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    grad_x = cv2.filter2D(gray,-1,mask_x)
+    grad_y = cv2.filter2D(gray,-1,mask_y)
+
+    grad_x_scaled = np.uint8(grad_x /2 + 128)
+    grad_y_scaled = np.uint8(grad_y / 2 +128)
+
+    cv2.imshow('Gradiente en x', grad_x_scaled)
+    cv2.waitKey(0)
+    cv2.imshow('Gradiente en y', grad_y_scaled)
+    cv2.waitKey(0)
+
 
 
 cap = cv2.VideoCapture(0)
 img = cv2.imread('poster.pgm', cv2.IMREAD_COLOR)
 
-SobelOperator(img)
+#SobelOperator(img)
+cannyOperator(img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
