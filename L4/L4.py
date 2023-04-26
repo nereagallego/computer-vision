10df39a58f1712be7b46fc716f415175d6e53147
Nerea <801950@unizar.es>
2023-04-26 16:34:33 +0200

    parameters and documentary

--- L4/L4.py
+++ L4/L4.py
@@ -6,10 +6,16 @@ from matplotlib import pyplot as plt
 import time
 
 # https://docs.opencv.org/4.x/da/df5/tutorial_py_sift_intro.html
-"""
-    Devuelve los keypoints, los descriptores, el tiempo de detección y la cantidad de características
-"""
-def SIFT_keypoints(gray, nfeatures):
+
+    """_summary_ Devuelve los keypoints, los descriptores, el tiempo de detección y la cantidad de características
+
+    Args:
+        nfeatures: number of features returned
+        contrstThreshold: filter out weak features in semi-uniform (low-contrast) regions. The larger the threshold, the less features are produced by the detector.
+        sigma: The sigma of the Gaussian applied to the input image at the octave #0
+
+    """
+def SIFT_keypoints(gray, nfeatures : int, contrastThreshold=0.04, sigma=1.6):
     start = time.time()
     sift = cv2.SIFT_create(nfeatures)
     kp, desc = sift.detectAndCompute(gray,None)
@@ -18,47 +24,64 @@ def SIFT_keypoints(gray, nfeatures):
     
 
 # https://docs.opencv.org/3.4/dc/d0d/tutorial_py_features_harris.html
-def HARRIS_keypoints(gray):
+    """_summary_ Devuelve los puntos de HARRYS
+        Args:
+
+            blockSize:	Neighborhood size
+            ksize:	Aperture parameter for the Sobel operator.
+            k:	Harris detector free parameter.
+    """
+def HARRIS_keypoints(gray, blockSize=2, ksize = 3, k = 0.04):
     
-    dst = cv2.cornerHarris(gray,2,3,0.04)
+    dst = cv2.cornerHarris(gray,blockSize, ksize , k)
     #result is dilated for marking the corners, not important
     dst = cv2.dilate(dst,None)
     
     return dst
 
 # https://docs.opencv.org/3.4/d1/d89/tutorial_py_orb.html
-def ORB_keypoints(gray, nfeatures):
+    """_summary_ Devuelve los keypoints, los descriptores, el tiempo de detección y la cantidad de características
+        Args:
+
+            nfeatures: number of features returned
+            edgeThreshold:	This is size of the border where the features are not detected. 
+    """
+def ORB_keypoints(gray, nfeatures, edgeThreshold = 31):
 
     # Initiate ORB detector
-    orb = cv2.ORB_create(nfeatures)
+    start = time.time()
+    orb = cv2.ORB_create(nfeatures,	edgeThreshold = edgeThreshold)
     # find the keypoints with ORB
     kp = orb.detect(gray,None)
     # compute the descriptors with ORB
     kp, des = orb.compute(gray, kp)
+    end = time.time()
     # draw only keypoints location,not size and orientation
-    return kp, des
+    return kp, des.astype(np.float32), end - start, len(kp)
 
+
+    """_summary_ Devuelve los keypoints, los descriptores, el tiempo de detección y la cantidad de características
+    """
 def AKAZE_keypoints(gray):
+    start = time.time()
     akaze = cv2.AKAZE_create()
     kpts1, desc1 = akaze.detectAndCompute(gray, None)
-    return kpts1
+    end = time.time()
+    return kpts1, desc1.astype(np.float32), end - start, len(kpts1)
 
 # https://docs.opencv.org/3.4/d3/da1/classcv_1_1BFMatcher.html
-"""
-    L1 and L2 norms are preferable choices for SIFT and SURF descriptors, NORM_HAMMING should be used with ORB, BRISK and BRIEF, NORM_HAMMING2 should be used with ORB 
-"""
-"""
-    Devuelve el tiempo de emparejamiento y la cantidad de emparejamientos
-"""
+    """ _summary_ L1 and L2 norms are preferable choices for SIFT and SURF descriptors, NORM_HAMMING should be used with ORB, BRISK and BRIEF, NORM_HAMMING2 should be used with ORB 
+    """
+    """ _summary_
+        Devuelve el tiempo de emparejamiento y la cantidad de emparejamientos
+    """
 def bruteForce(img1,kp1, desc1,img2, kp2, desc2):
     start = time.time()
     matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_BRUTEFORCE_L1)
     nn_matches = matcher.knnMatch(desc1, desc2, 2)
     end = time.time()
     time_emparejamiento = end - start
-#    matches = sorted(matches, key = lambda x:x.distance)
- #   img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches,None,flags=2)
-    
+        
     matched1 = []
     matched2 = []
     matches = []
@@ -74,13 +97,6 @@ def bruteForce(img1,kp1, desc1,img2, kp2, desc2):
     plt.imshow(img3),plt.show()
     #plt.imshow(img3),plt.show()
     return time_emparejamiento, num_emparejamientos
-
-img = cv2.imread('BuildingScene/building1.JPG')
-gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-# kp = SIFT_keypoints(gray)
-# img=cv2.drawKeypoints(gray,kp,img)
-# cv2.imwrite('sift_keypoints.jpg',img)
-
 
 # https://docs.opencv.org/4.x/dc/dc3/tutorial_py_matcher.html
 def flann_Matching(img1,kp1, desc1,img2, kp2, desc2):
@@ -107,6 +123,15 @@ def flann_Matching(img1,kp1, desc1,img2, kp2, desc2):
     plt.imshow(img3,),plt.show()
     return time_emparejamiento, len(matches)
 
+img = cv2.imread('BuildingScene/building1.JPG')
+gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# kp = SIFT_keypoints(gray)
+# img=cv2.drawKeypoints(gray,kp,img)
+# cv2.imwrite('sift_keypoints.jpg',img)
+
+
+
+
 
 
 dst = HARRIS_keypoints(gray)
@@ -130,10 +155,11 @@ dst = HARRIS_keypoints(gray)
 img2 = cv2.imread('BuildingScene/building2.JPG')
 gray2= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-kp1, desc1, time_detection, num_features = SIFT_keypoints(gray,500)
-kp2, desc2, time_detection2, num_features2  = SIFT_keypoints(gray2,500)
+kp1, desc1, time_detection, num_features = ORB_keypoints(gray,500)
+kp2, desc2, time_detection2, num_features2  = ORB_keypoints(gray2,500)
 time1, num_matches1 = flann_Matching(gray, kp1, desc1, gray2, kp2, desc2)
 time2, num_matches2 = bruteForce(gray, kp1, desc1, img2, kp2, desc2)
 
 print(time1, ' ', time2)
+print(num_matches1, ' ', num_matches2)
 
